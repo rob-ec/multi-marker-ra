a3102bc806d9296e678a92276d3d03944b733bdd
Robson Mesquita Gomes <robson.mesquita56@gmail.com>
2026-03-03 20:24:15 -0300

    🚀 init test

---
+++ assets/js/characters.js
@@ -0,0 +1,26 @@
+const characters = [
+    {
+        name: "Goblin A",
+        model: "goblin.glb",
+        marker: {
+            patt: "goblin-a.patt",
+            image: "goblin-a.png",
+        },
+    },
+    {
+        name: "Goblin B",
+        model: "purple_goblin.glb",
+        marker: {
+            patt: "goblin-b.patt",
+            image: "goblin-b.png",
+        },
+    },
+    {
+        name: "Wizard",
+        model: "elf_wizard.glb",
+        marker: {
+            patt: "wand.patt",
+            image: "wand.png",
+        },
+    },
+];
